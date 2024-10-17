2838b3368bba4c9d2a21a693cd68e92ffba61272
bistasushant <155437951+bistasushant@users.noreply.github.com>
2024-10-17 18:43:47 +0545

    Add files via upload

---
+++ src/sections/Header.tsx
@@ -0,0 +1,44 @@
+import LogoIcon from "@/assets/logo.svg";
+import {Button} from "@/components/Button"
+export const Header = () => {
+  return (
+    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+        <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
+          <div>
+  
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+              <LogoIcon className="w-8 h-8" />
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm">
+              <a href="#" className="text-white/70 hover:text-white transition">Features</a>
+              <a href="#" className="text-white/70 hover:text-white transition">Developers</a>
+              <a href="#" className="text-white/70 hover:text-white transition">Pricing</a>
+              <a href="#" className="text-white/70 hover:text-white transition">Changelog</a>
+            </nav>
+          </div>
+          <div className="flex gap-4 items-center">
+     
+            <Button>Join waitlist</Button>
+            <svg
+              className="md:hidden"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M4 6H20" stroke="currentColor" strokeWidth="2" />
+              <path d="M4 12H20" stroke="currentColor" strokeWidth="2" />
+              <path d="M4 18H20" stroke="currentColor" strokeWidth="2" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
